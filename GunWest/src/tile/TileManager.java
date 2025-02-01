0eb08c234aaf61869cdde31fa71dace470df061b
YLooM <yloomlebg@gmail.com>
2025-02-01 15:32:02 -0500

    Tile

---
+++ GunWest/src/tile/TileManager.java
@@ -0,0 +1,126 @@
+package tile;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import javax.imageio.ImageIO;
+
+import main.GamePanel;
+
+
+public class TileManager {
+	
+	GamePanel gp;
+	public Tile[] tile;
+	public int mapTileNumber[][];
+	
+	public TileManager(GamePanel gp) {
+		this.gp = gp;
+		tile = new Tile[50];
+		
+		mapTileNumber = new int[gp.maxWorldCol][gp.maxWorldRow];
+		getTileImage();
+		loadMap("/maps/worldV22.txt");
+	}
+	
+	//CHarger les tuiles
+	public void getTileImage() {
+			// TILE PARTIE DESERT
+			
+			setup(0, "Desert1", false);
+			setup(1, "Desert2", true);
+			setup(10, "Desert3", true);
+			
+
+	}
+	//Charger les images
+	
+	public void setup(int index, String imageName, boolean collision) {
+	    try {
+	        tile[index] = new Tile();
+	        File imageFile = new File("ressources/tile/" + imageName + ".png");
+	        tile[index].image = ImageIO.read(imageFile);
+	        tile[index].collision = collision;
+
+	    } catch(IOException e) {
+	        e.printStackTrace();
+	    }
+	}
+	
+	
+	//CHARGER LA MAP
+	public void loadMap(String filePath) {
+		try {
+			InputStream is = getClass().getResourceAsStream(filePath);
+			BufferedReader br = new BufferedReader(new InputStreamReader(is));
+			
+			int col = 0;
+			int row = 0;
+			
+			while (col < gp.maxWorldCol && row < gp.maxWorldRow) {
+				String line = br.readLine();
+				
+				while (col  < gp.maxWorldCol) {
+					String numbers[] = line.split(" ");
+					
+					int num = Integer.parseInt(numbers[col]);
+					
+					mapTileNumber[col][row] = num;
+					col++;
+					
+				}
+				if (col == gp.maxWorldCol) {
+					col = 0;
+					row++;
+				}
+			}
+			br.close();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+	//DESSINER LA MAP
+	
+	public void draw(Graphics2D g2) {
+		int worldRow = 0;
+		int worldCol = 0;
+
+		
+		while (worldCol < gp.maxWorldCol && worldRow < gp.maxWorldRow) {
+			int tileNum = mapTileNumber[worldCol][worldRow];
+			
+			int worldX = worldCol * gp.tileSize;
+			int worldY = worldRow * gp.tileSize;
+			int screenX = worldX - gp.player.worldX + gp.player.screenX;
+			int screenY = worldY - gp.player.worldY + gp.player.screenY;
+			
+			if (worldX + gp.tileSize > gp.player.worldX - gp.player.screenX && 
+				worldX - gp.tileSize < gp.player.worldX + gp.player.screenX &&
+				worldY + gp.tileSize > gp.player.worldY - gp.player.screenY && 
+				worldY - gp.tileSize < gp.player.worldY + gp.player.screenY ) {
+				
+				g2.drawImage(tile[tileNum].image, screenX, screenY, null);
+			}
+			
+			worldCol++;
+			
+			if (worldCol == gp.maxWorldCol) {
+				worldCol = 0;
+				worldRow++;
+			}
+			
+		}
+			
+		}
+		
+	}
+
