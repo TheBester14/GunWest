547f29ca5f5e7caacb3044fdd36bed8b2b78ee3c
Adnane <adnane552@hotmail.ca>
2025-02-01 17:35:24 -0500

    Merge completed main and ralph branch

--- GunWest/src/entities/NPC.java
+++ GunWest/src/entities/NPC.java
@@ -1,24 +1,17 @@
 package entities;
 
 import main.GamePanel;
-<<<<<<< HEAD
+
 import main.KeyHandler;
 
 public class NPC extends Entity {
 	public NPC() {
 		
 	}
-	
-	public void update() {
+
+	@Override
+	void update() {
+		// TODO Auto-generated method stub
 		
-=======
-
-public class NPC extends Entity {
-
-	public NPC(GamePanel gp) {
-		super(gp);
-		// TODO Auto-generated constructor stub
->>>>>>> ralph
 	}
-
-}
+}	
