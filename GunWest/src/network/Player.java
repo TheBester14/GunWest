900fa81af2ced158e5aec2909626a51090e48134
unknown <hammama.amine@gmail.com>
2025-02-01 15:19:19 -0500

    Changes to the Player class for multiplayer p2p

--- GunWest/src/network/Player.java
+++ GunWest/src/network/Player.java
@@ -1,5 +1,30 @@
 package network;
 
-public class Player {
+import java.io.*;
+import java.net.Socket;
 
+public class Player {
+    private Socket socket;
+    private BufferedReader input;
+    private PrintWriter output;
+
+    public Player(Socket socket) throws IOException {
+        this.socket = socket;
+        this.input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        this.output = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    public void sendMessage(String message) {
+        output.println(message);
+    }
+
+    public String receiveMessage() throws IOException {
+        return input.readLine();
+    }
+
+    public void close() throws IOException {
+        input.close();
+        output.close();
+        socket.close();
+    }
 }
