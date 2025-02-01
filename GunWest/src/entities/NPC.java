1ddbea3c9154300b8f051b7140c570abb20b93c9
Adnane <adnane552@hotmail.ca>
2025-02-01 17:30:55 -0500

    merged

--- GunWest/src/entities/NPC.java
+++ GunWest/src/entities/NPC.java
@@ -1,6 +1,7 @@
 package entities;
 
 import main.GamePanel;
+<<<<<<< HEAD
 import main.KeyHandler;
 
 public class NPC extends Entity {
@@ -10,5 +11,14 @@ public class NPC extends Entity {
 	
 	public void update() {
 		
+=======
+
+public class NPC extends Entity {
+
+	public NPC(GamePanel gp) {
+		super(gp);
+		// TODO Auto-generated constructor stub
+>>>>>>> ralph
 	}
+
 }
