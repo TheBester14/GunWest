00e08d5ddddc1ac92ffd0cc0a778df1e6574538f
Hendrik Tebeng <hnt.nzodoum@gmail.com>
2025-02-01 14:03:15 -0500

    Create skeleton methods for Entity class.

--- GunWest/src/entities/Entity.java
+++ GunWest/src/entities/Entity.java
@@ -1,5 +1,31 @@
 package entities;
 
-public class Entity {
-
+public abstract class Entity {
+	private int x;
+	private int y;
+	private int width;
+	private int height;
+	private int speed;
+	private int health;
+	
+	public Entity(int x, int y, String path) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void takeDamage() {
+		
+	}
+	
+	public void draw() {
+		
+	}
+	
+	public void update() {
+		
+	}
 }
