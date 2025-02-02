2dc99c4aab14c89835121aea434bc215044f45ab
Hendrik Tebeng <hnt.nzodoum@gmail.com>
2025-02-01 20:59:12 -0500

    Create UI skeleton class.

---
+++ GunWest/src/main/UI.java
@@ -0,0 +1,36 @@
+package main;
+
+import java.awt.Graphics;
+import entities.Player;
+
+public class UI {
+	GamePanel gp;
+	private Player player;
+	private int[] score;
+	private int hp;
+	
+	public UI(GamePanel gp, Player player) {
+		this.gp = gp;
+		this.player = player;
+		this.lives = 3;
+		this.score = new Int{0, 0};
+	}
+	
+	private void drawLives(Graphics g) {
+		
+	}
+	
+	private void drawGuns(Graphics g) {
+		
+	}
+	
+	private void drawScore(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public 
+}
