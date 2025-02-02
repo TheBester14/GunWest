b2f31a04538112cabbdaaf5dbe97b31b62033c0b
Hendrik Tebeng <hnt.nzodoum@gmail.com>
2025-02-01 21:21:27 -0500

    Adjust getters and setteers.

--- GunWest/src/entities/Entity.java
+++ GunWest/src/entities/Entity.java
@@ -14,12 +14,7 @@ public abstract class Entity {
     protected Color color;
     protected int speed;
     
-    // Sprite images for various directions (if needed)
-    protected BufferedImage up1, up2, down, left, right, upRight, upLeft, downRight, downLeft;
-    protected BufferedImage down1, down2;
-    
     public Entity() {
-        // Default starting position and size
         this.x = 500;
         this.y = 500;
         this.width = 50;
@@ -91,93 +86,5 @@ public abstract class Entity {
 
 	public void setSpeed(int speed) {
 		this.speed = speed;
-	}
-
-	public BufferedImage getUp1() {
-		return up1;
-	}
-
-	public void setUp1(BufferedImage up1) {
-		this.up1 = up1;
-	}
-
-	public BufferedImage getUp2() {
-		return up2;
-	}
-
-	public void setUp2(BufferedImage up2) {
-		this.up2 = up2;
-	}
-
-	public BufferedImage getDown() {
-		return down;
-	}
-
-	public void setDown(BufferedImage down) {
-		this.down = down;
-	}
-
-	public BufferedImage getLeft() {
-		return left;
-	}
-
-	public void setLeft(BufferedImage left) {
-		this.left = left;
-	}
-
-	public BufferedImage getRight() {
-		return right;
-	}
-
-	public void setRight(BufferedImage right) {
-		this.right = right;
-	}
-
-	public BufferedImage getUpRight() {
-		return upRight;
-	}
-
-	public void setUpRight(BufferedImage upRight) {
-		this.upRight = upRight;
-	}
-
-	public BufferedImage getUpLeft() {
-		return upLeft;
-	}
-
-	public void setUpLeft(BufferedImage upLeft) {
-		this.upLeft = upLeft;
-	}
-
-	public BufferedImage getDownRight() {
-		return downRight;
-	}
-
-	public void setDownRight(BufferedImage downRight) {
-		this.downRight = downRight;
-	}
-
-	public BufferedImage getDownLeft() {
-		return downLeft;
-	}
-
-	public void setDownLeft(BufferedImage downLeft) {
-		this.downLeft = downLeft;
-	}
-
-	public BufferedImage getDown1() {
-		return down1;
-	}
-
-	public void setDown1(BufferedImage down1) {
-		this.down1 = down1;
-	}
-
-	public BufferedImage getDown2() {
-		return down2;
-	}
-
-	public void setDown2(BufferedImage down2) {
-		this.down2 = down2;
 	}
 }
